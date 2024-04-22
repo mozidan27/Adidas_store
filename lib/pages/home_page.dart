e6f701b4e06d02c202b526f3e50136aece9d0538
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-04-22 08:53:41 +0200

    finishing the navbar and bulit the pages list but still not finish yet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:adidas/pages/Cart_page.dart';
+import 'package:adidas/pages/Shop_page.dart';
+import 'package:adidas/widgets/gnav_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +11,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // this is the selected index to select the index of navbar
+
+  int _selectedindex = 0;
+// this method to navitate the  pages
+  void navigateBottomBar(int indext) {
+    setState(() {
+      _selectedindex = indext;
+    });
+  }
+// these are the pages we will navigate to
+
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyButtomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedindex],
+    );
   }
 }
---
+++ lib/widgets/gnav_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyButtomNavBar extends StatelessWidget {
+  MyButtomNavBar({super.key, required this.onTabChange});
+  void Function(int)? onTabChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: GNav(
+          onTabChange: (value) => onTabChange!(value),
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBackgroundColor: Colors.grey.shade100,
+          // backgroundColor: Colors.white,
+          activeColor: Colors.grey[800],
+          tabBorderRadius: 16,
+          color: Colors.grey,
+          tabs: const [
+            GButton(
+              icon: Icons.shop,
+              text: 'Shop',
+            ),
+            GButton(
+              icon: Icons.shopping_bag_rounded,
+              text: 'Cart',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
